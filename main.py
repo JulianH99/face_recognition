1189c9b0551934b19103f4f0e3175fff79cef1c0
JulianH99 <juliancorredors99@outlook.com>
2020-08-11 16:42:06 -0500

    tiene opencv

--- main.py
+++ main.py
@@ -8,13 +8,13 @@ Created on Sat Aug  8 11:22:28 2020
 
 
 
-import tensorflow as tf
-import keras as khe
+
+
 
 import csv
 
 import os
-
+from PIL import Image
 from tensorflow.keras.preprocessing.image import ImageDataGenerator, load_img, img_to_array
 from tensorflow.keras.models import Sequential, load_model
 from tensorflow.keras.layers import Dense, Dropout, Flatten, Activation
@@ -23,8 +23,11 @@ from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
 from tensorflow.keras import backend as K
 from tensorflow.keras.utils import to_categorical
 import numpy as np
+import cv2
 
 
+
+face_cascade = cv2.CascadeClassifier('./cascade.xml')
 
 def create_emotion_folder(emotion):
     emotions_folder = os.path.join(os.getcwd(), 'emotions')
@@ -174,6 +177,62 @@ def use_tf():
     
     print(y_pred_prob)
     
+    
+    videocam(model_loaded)
+    
+    
+    
+
+def face_extractor(img):
+    # Function detects faces and returns the cropped face
+    # If no face detected, it returns the input image
+    
+    #gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(img, 1.3, 5)
+    
+    if faces is ():
+        return None
+    
+    # Crop all faces found
+    for (x,y,w,h) in faces:
+        cv2.rectangle(img,(x,y),(x+w,y+h),(0,350,350),2)
+        cropped_face = img[y:y+h, x:x+w]
+
+    return cropped_face
+    
+
+
+def videocam(model):
+    video_capture = cv2.VideoCapture(0)
+    while True:
+        _, frame = video_capture.read()
+        #canvas = detect(gray, frame)
+        #image, face =face_detector(frame)
+        
+        face=face_extractor(frame)
+        if type(face) is np.ndarray:
+            face = cv2.resize(face, (350, 350))
+            im = Image.fromarray(face, 'RGB')
+               #Resizing into 128x128 because we trained the model with this image size.
+            img_array = np.array(im)
+                        #Our keras model used a 4D tensor, (images x height x width x channel)
+                        #So changing dimension 128x128x3 into 1x128x128x3 
+            img_array = np.expand_dims(img_array, axis=0)
+            pred = model.predict(img_array)
+            print(pred)
+                         
+            name="None matching"
+            
+            if(pred[0][3]>0.5):
+                name='Krish'
+            #cv2.putText(frame,name, (50, 50), cv2.FONT_HERSHEY_COMPLEX, 1, (0,350,0), 2)
+        else:
+            cv2.putText(frame,"No face found", (50, 50), cv2.FONT_HERSHEY_COMPLEX, 1, (0,350,0), 2)
+        cv2.imshow('Video', frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    video_capture.release()
+    cv2.destroyAllWindows()
     
 classify()
 
